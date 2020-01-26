9540e22729cd5c740921cdd95e92442dca1a0015
tdude92 <trevorguanhao@gmail.com>
2020-01-26 06:34:08 +0000

    push

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import numpy as np
 MODEL_ID        = "0"
 DATA_PATH       = "data"
 
-START_EPOCH     = 1
-N_EPOCHS        = 750
+START_EPOCH     = 161
+N_EPOCHS        = 1000
 LEN_Z           = 100
 OUT_CHANNELS    = 3
 IMAGE_DIM       = 64
